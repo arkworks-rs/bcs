52b8dadce2146fddb72a5ee728011d692b6b984d
Tom Shen <tomshen@berkeley.edu>
2021-07-08 22:44:55 -0700

    start work

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! A crate for interactive oracle proofs and BCS transform.
+#![deny(unused_import_braces, unused_qualifications, trivial_casts)]
+#![deny(trivial_numeric_casts, private_in_public, variant_size_differences)]
+#![deny(stable_features, unreachable_pub, non_shorthand_field_patterns)]
+#![deny(unused_attributes, unused_mut)]
+#![deny(missing_docs)]
+#![deny(unused_imports)]
+#![deny(renamed_and_removed_lints, stable_features, unused_allocation)]
+#![deny(unused_comparisons, bare_trait_objects, unused_must_use, const_err)]
+#![forbid(unsafe_code)]
